f49956753e1d6aee2807a6dec6b2cdab209c3c66
Stiven Ramírez Arango <stivenramirez@habi.co>
2022-06-11 12:55:30 -0500

    Testing ecommerce

---
+++ tests/test_ecommerce.py
@@ -0,0 +1,26 @@
+import unittest
+from pyunitreport import HTMLTestRunner
+from selenium import webdriver
+
+from src.secrets import DRIVER_PATH
+
+
+class EcommerceTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+        self.driver.implicitly_wait(10)
+        self.driver.get('http://demo-store.seleniumacademy.com')
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(15)
+
+    def tearDown(self) -> None:
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main(
+        verbosity=2,
+        testRunner=HTMLTestRunner(
+            output='reports', report_name='ecommerce-report'
+        ),
+    )
