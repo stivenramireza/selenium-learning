74829ca12ef3ed5b7b573dc6efabca928776d0bb
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-06-12 16:42:27 -0500

    [ADD] Navigation test case

---
+++ tests/test_navigation.py
@@ -0,0 +1,17 @@
+from tests.test_ecommerce import EcommerceTestCase
+
+
+class NavigationTestCase(EcommerceTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.driver.get('https://www.google.com')
+
+    def test_browser_navigation(self) -> None:
+        search_field = self.driver.find_element_by_name('q')
+        search_field.clear()
+        search_field.send_keys('platzi')
+        search_field.submit()
+
+        self.driver.back()
+        self.driver.forward()
+        self.driver.refresh()
