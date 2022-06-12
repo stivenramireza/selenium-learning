cbedef09866019c96a7ffbd1e0c1760258722ab9
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-06-12 15:37:46 -0500

    Test assertions

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -3,16 +3,16 @@ from tests.test_ecommerce import EcommerceTestCase
 
 class HomePageTestCase(EcommerceTestCase):
     def test_search_text_field(self) -> None:
-        search_field = self.driver.find_element_by_id('search')
+        self.driver.find_element_by_id('search')
 
     def test_search_text_field_by_name(self) -> None:
-        search_field = self.driver.find_element_by_name('q')
+        self.driver.find_element_by_name('q')
 
     def test_search_text_field_by_class_name(self) -> None:
-        search_field = self.driver.find_element_by_class_name('input-text')
+        self.driver.find_element_by_class_name('input-text')
 
     def test_search_button_enabled(self) -> None:
-        button = self.driver.find_element_by_class_name('button')
+        self.driver.find_element_by_class_name('button')
 
     def test_count_of_promo_banner_images(self) -> None:
         banner_list = self.driver.find_element_by_class_name('promos')
@@ -20,11 +20,11 @@ class HomePageTestCase(EcommerceTestCase):
         self.assertEqual(len(banners), 3)
 
     def test_vip_promo(self) -> None:
-        vip_promo = self.driver.find_element_by_xpath(
+        self.driver.find_element_by_xpath(
             '//*[@id="top"]/body/div/div[2]/div[2]/div/div/div[2]/div[1]/ul/li[4]/a/img'
         )
 
     def test_shopping_cart(self) -> None:
-        shopping_cart_icon = self.driver.find_element_by_css_selector(
+        self.driver.find_element_by_css_selector(
             'div.header-minicart span.icon'
         )
